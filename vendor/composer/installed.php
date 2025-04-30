c8b086a2b18e8bce1fe2499438a8f83e19893358
Jacem Jouili <jacem.jouili@esprit.tn>
2025-04-30 02:00:33 +0100

    Quelque modification de design Notifiactions

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '7ac674cf389caa3ee82b673c6ee7b28ab4b093d4',
+        'reference' => '80ff9cbc19075cef10f3483523f84b96ed679cf2',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '7ac674cf389caa3ee82b673c6ee7b28ab4b093d4',
+            'reference' => '80ff9cbc19075cef10f3483523f84b96ed679cf2',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
